2131f4390477001c1b1130b6d91016a65a0c3e20
gwoprk <gwoprk98@gmail.com>
2024-10-01 13:49:37 +0900

    백준 문제풀이

---
+++ BOJ/문자열/P1032.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class P1032 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        char[] pattern = br.readLine().toCharArray();
+
+        for (int i = 0; i < N - 1; i++) {
+            char[] filename = br.readLine().toCharArray();
+            for (int j = 0; j < pattern.length; j++) {
+                if (pattern[j] != filename[j]) pattern[j] = '?';
+            }
+        }
+        System.out.println(pattern);
+
+    }
+}
