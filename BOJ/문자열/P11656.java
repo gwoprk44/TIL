7779f9ad7df50d2657b60ec8960f300ede0276c4
gwoprk <gwoprk98@gmail.com>
2024-10-04 11:01:25 +0900

    백준 문제풀이

---
+++ BOJ/문자열/P11656.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class P11656 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[] strArr = new String[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            strArr[i] = s.substring(i, s.length());
+        }
+        Arrays.sort(strArr);
+
+        for (String str : strArr) {
+            System.out.println(str);
+        }
+    }
+}
