7fe7798e671b43e092c9aa1cbcb4ab980ac3708c
gwoprk <gwoprk98@gmail.com>
2024-11-05 22:15:05 +0900

    코테

---
+++ CodingTest/BOJ/입출력과사칙연산/P10926.java
@@ -0,0 +1,12 @@
+package BOJ.입출력과사칙연산;
+
+import java.io.*;
+
+public class P10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        System.out.println(s + "??!");
+    }
+}
