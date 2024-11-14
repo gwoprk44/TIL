a4bf122a0167ffef06e94e2c5973f1692680538b
gwoprk98 <gwoprk98@gmail.com>
2024-11-14 12:01:37 +0900

    코테

---
+++ CodingTest/BOJ/브루트포스/P19532.java
@@ -0,0 +1,23 @@
+package CodingTest.BOJ.브루트포스;
+
+import java.io.*;
+import java.util.*;
+
+public class P19532 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+        
+        int x = (e * c - b * f) / (e * a - b * d);
+        int y = (c * d - a * f) / (d * b - a * e);
+
+        System.out.println(x + " " + y);
+    }
+}
---
+++ CodingTest/BOJ/브루트포스/P2231.java
@@ -0,0 +1,28 @@
+package CodingTest.BOJ.브루트포스;
+
+import java.io.*;
+
+public class P2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            int number = i;
+            int sum = 0;
+
+            while (number != 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            if (sum + i == N) {
+                answer = i;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+}
