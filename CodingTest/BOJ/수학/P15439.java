fb82c12b9dd79870372322d16fd50f1418d27aa4
gwoprk98 <gwoprk98@gmail.com>
2024-11-21 10:53:34 +0900

    코딩테스트

---
+++ CodingTest/BOJ/수학/P15439.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+
+public class P15439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N * (N - 1));   
+    }
+    
+}
