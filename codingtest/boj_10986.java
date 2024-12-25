fea49f4a51085338d4a142c13ebb2799bbbacf08
gwoprk98 <gwoprk98@gmail.com>
2024-12-25 18:14:03 +0900

    feat: 코딩테스트

---
+++ codingtest/boj_10986.java
@@ -0,0 +1,37 @@
+package codingtest;
+
+import java.io.*;
+import java.util.*;
+
+public class boj_10986 {
+    private void solution() throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int sum = 0;
+        int[] cnt = new int[1000];
+        long answer = 0;
+
+        st = new StringTokenizer(br.readLine());
+
+        while (n --> 0) {
+            int cur = Integer.parseInt(st.nextToken());
+            sum += cur;
+            sum %= m;
+            answer += cnt[sum];
+            cnt[sum]++;
+            if (sum == 0) answer++;
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws Exception{
+        new boj_10986().solution();
+
+    }
+}
