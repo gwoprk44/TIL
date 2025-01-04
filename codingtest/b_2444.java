ea7b103f5170c1a01884fdb7a89e5628c532f5a0
gwoprk98 <gwoprk98@gmail.com>
2025-01-04 13:10:40 +0900

    style: 코드제거

--- codingtest/b_2444.java
+++ codingtest/b_2444.java
@@ -2,7 +2,6 @@ package codingtest;
 
 
 import java.io.*;
-import java.util.*;
 public class b_2444 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
