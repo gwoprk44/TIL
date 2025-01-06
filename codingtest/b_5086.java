57b69eeb45aad924b2a4ff44480bcf560aa9b1d7
gwoprk98 <gwoprk98@gmail.com>
2025-01-06 13:30:22 +0900

    feat: 코딩 테스트

---
+++ codingtest/b_5086.java
@@ -0,0 +1,26 @@
+package codingtest;
+
+import java.io.*;
+import java.util.*;
+
+public class b_5086 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) break;
+
+            if (b % a == 0) {
+                System.out.println("factor");
+            } else if (a % b == 0) {
+                System.out.println("multiple");
+            } else {
+                System.out.println("neither");
+            }
+        }
+    }
+}
