552b63bc83ea381e893f689e19a48e5c3add7291
gwoprk98 <gwoprk98@gmail.com>
2025-01-10 22:29:32 +0900

    feat: 코딩 테스트

---
+++ codingtest/b_24265.java
@@ -0,0 +1,17 @@
+package codingtest;
+
+import java.io.*;
+
+
+public class b_24265 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Integer.parseInt(br.readLine());
+
+        System.out.println((n*(n-1))/2);
+        System.out.println(2);
+
+    }    
+}
