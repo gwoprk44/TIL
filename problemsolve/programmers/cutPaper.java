e66673e34a425ded838f0c90db452990b8154880
gwoprk98 <gwoprk98@gmail.com>
2025-01-15 11:49:28 +0900

    feat: 코딩 테스트

---
+++ problemsolve/programmers/cutPaper.java
@@ -0,0 +1,8 @@
+package problemsolve.programmers;
+
+class Solution {
+    public int solution(int M, int N) {
+        
+        return M * N - 1;
+    }
+}
---
+++ problemsolve/programmers/pushString.java
@@ -0,0 +1,8 @@
+package problemsolve.programmers;
+
+class Solution {
+    public int solution(String A, String B) {
+
+        return (B + B).indexOf(A);
+    }
+}
