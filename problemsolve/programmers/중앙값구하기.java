673382c6db5ca3371d14b286f3e9f7082895c86a
gwoprk98 <gwoprk98@gmail.com>
2025-01-21 11:31:33 +0900

    feat: PS

---
+++ problemsolve/programmers/중앙값구하기.java
@@ -0,0 +1,16 @@
+package problemsolve.programmers;
+
+import java.util.Arrays;
+
+class 중앙값구하기 {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        Arrays.sort(array);
+        answer = array[array.length/2];
+        
+        
+        return answer;
+
+    }
+}
---
+++ problemsolve/programmers/짝수는싫어요.java
@@ -0,0 +1,20 @@
+package problemsolve.programmers;
+
+class 짝수는싫어요 {
+    public int[] solution(int n) {
+        int[] answer;
+
+        if (n % 2 == 0) {
+            answer = new int[n / 2];
+        } else {
+            answer = new int[n / 2 + 1];
+        }
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = i * 2 + 1;
+        }
+       
+        
+        return answer;
+    }
+}
