ad2a21c7973d7a20956703ca53535b999753d4e8
gwoprk98 <gwoprk98@gmail.com>
2025-01-31 22:00:00 +0900

    feat: ps

---
+++ problemsolve/programmers/숫자찾기.java
@@ -0,0 +1,20 @@
+package problemsolve.programmers;
+
+class 숫자찾기 {
+    public int solution(int num, int k) {
+        int answer = -1;
+        
+        String str = Integer.toString(num);
+        String[] arr = str.split("");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(Integer.toString(k))) {
+                answer = i + 1;
+                break;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
