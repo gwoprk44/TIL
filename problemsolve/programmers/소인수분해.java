cf3bd90a944050dfcca3a01e2689dd3a2eae437b
gwoprk98 <gwoprk98@gmail.com>
2025-02-06 23:21:20 +0900

    feat: 코딩테스트

---
+++ problemsolve/programmers/소인수분해.java
@@ -0,0 +1,26 @@
+package problemsolve.programmers;
+
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                while (n % i == 0) {
+                    n /= i;
+                }
+                arr.add(i);
+            }
+        }
+
+        int[] answer = new int[arr.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = arr.get(i);
+        }
+
+        return answer;
+    }
+}
