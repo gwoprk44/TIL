0f7d9c177d2952d607c611976ecb71f0a5a48e54
gwoprk98 <gwoprk98@gmail.com>
2025-03-05 11:51:42 +0900

    feat: ps

---
+++ problemsolve/BOJ/알파벳_개수.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int alpha[26];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    for (auto c : s) {
+        alpha[c - 'a']++;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        cout << alpha[i] << ' ';
+    }
+}
