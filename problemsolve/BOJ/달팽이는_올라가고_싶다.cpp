aa75f0f1bcefe93a92c5318b8eda7db3e04d463c
gwoprk98 <gwoprk98@gmail.com>
2025-06-19 09:39:51 +0900

    feat: ps

---
+++ problemsolve/BOJ/달팽이는_올라가고_싶다.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b, v;
+    cin >> a >> b >> v;
+
+    int days = (v-b-1) / (a-b) + 1;
+
+    cout << days;
+}
