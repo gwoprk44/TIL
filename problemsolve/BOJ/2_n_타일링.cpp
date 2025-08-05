0175c2f31a346f0135af636f3a59b99cc2c5b17d
gwoprk98 <gwoprk98@gmail.com>
2025-08-05 13:10:44 +0900

    feat: ps

---
+++ problemsolve/BOJ/2_n_타일링.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> dp(n+1);
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+    }
+
+    cout << dp[n];
+}
