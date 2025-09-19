d0867f989ea804250537b71f52f5442b68a4cf8b
gwoprk98 <gwoprk98@gmail.com>
2025-09-19 11:29:15 +0900

    feat: ps

--- problemsolve/BOJ/다이얼.cpp
+++ problemsolve/BOJ/다이얼.cpp
@@ -5,15 +5,19 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    string s;
-    cin >> s;
+    int time[26] = { 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10 };
 
-    int ans = 0;
-    int time[26] = {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10};
+	string s;
+	int ans = 0;
 
-    for (int i = 0; i < s.length(); i++) {
-        ans += time[s[i] - 'A'];
-    }
-    
-    cout << ans;
+	cin >> s;
+
+	for (int i = 0; i < s.length(); i++) {
+		ans += time[s[i] - 'A'];
+	}
+	
+	cout << ans;
+	
+
+	return 0;
 }
