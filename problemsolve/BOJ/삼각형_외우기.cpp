adbc4b41bd9950831c67025ddf2bace0d1ae0e59
gwoprk98 <gwoprk98@gmail.com>
2025-10-04 13:26:29 +0900

    feat: ps

--- problemsolve/BOJ/삼각형_외우기.cpp
+++ problemsolve/BOJ/삼각형_외우기.cpp
@@ -5,10 +5,10 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int a, b, c; 
+    int a, b, c;
     cin >> a >> b >> c;
 
-    if (a+b+c != 180) {
+    if (a + b + c != 180 || a <= 0 || b <= 0 || c <= 0) { 
         cout << "Error";
     } else if (a == 60 && b == 60 && c == 60) {
         cout << "Equilateral";
--- problemsolve/BOJ/삼각형과_세_변.cpp
+++ problemsolve/BOJ/삼각형과_세_변.cpp
@@ -5,23 +5,25 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int a,b,c;
+    int a, b, c;
 
-    while (true) {
+    while (true)
+    {
         cin >> a >> b >> c;
         if (a == 0 && b == 0 && c == 0) break;
 
         int sides[3] = {a,b,c};
         sort(sides, sides+3);
 
-        if (sides[2] >= sides[0] + sides[1]) {
+        if (sides[2] >= sides[1] + sides[0]) {
             cout << "Invalid\n";
         } else if (a == b && a == c) {
             cout << "Equilateral\n";
-        } else if (a == b || a == c || b == c) {
+        } else if (a == b || b == c || a == c) {
             cout << "Isosceles\n";
         } else {
             cout << "Scalene\n";
         }
     }
+    
 }
