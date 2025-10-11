3851d850c3ab560493fa8bec0d41a8a665fcf0d6
gwoprk98 <gwoprk98@gmail.com>
2025-10-11 17:49:12 +0900

    feat: ps

--- problemsolve/BOJ/블랙잭.cpp
+++ problemsolve/BOJ/블랙잭.cpp
@@ -6,22 +6,26 @@ int main() {
     cin.tie(0);
 
     int n, m;
-    int cards[100];
     cin >> n >> m;
+
+    int card[100];
+
     for (int i = 0; i < n; i++) {
-        cin >> cards[i];
+        cin >> card[i];
     }
-    
+
     int ans = 0;
-    for (int i = 0; i < n-2; i++) {
-        for (int j = i+1; j < n-1; j++) {
-            for (int k = j+1; k < n; k++) {
-                int sum = cards[i] + cards[j] + cards[k];
+    
+    for (int i = 0; i < n - 2; i ++) {
+        for (int j = i + 1; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                int sum = card[i] + card[j] + card[k];
                 if (sum > ans && sum <= m) {
                     ans = sum;
                 }
             }
         }
     }
+
     cout << ans;
 }
