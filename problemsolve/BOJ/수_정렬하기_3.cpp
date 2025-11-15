11ad8739e5005c5b8a6eeaea49b055921f9a16d3
gwoprk44 <gwoprk98@gmail.com>
2025-11-15 16:31:36 +0900

    feat: ps

---
+++ problemsolve/BOJ/수_정렬하기_3.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int count[100001] = {0};
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        count[num]++;
+    }
+
+    for (int i = 1; i <= 10000; i++) {
+        for (int j = 0; j < count[i]; j++) {
+            cout << i << "\n";
+        }
+    }
+
+
+    return 0;
+}
