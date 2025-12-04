248c5b00a7f23aa4d0c3b62d12a9bef7dbdcdb70
cayman031 <cayman031@gmail.com>
2025-12-04 12:01:55 +0900

    feat: boj 반복문

---
+++ problemsolve/BOJ/반복문/A_B_4.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int a, b = 0;
+
+    while (cin >> a >> b) {
+        cout << a + b << "\n";
+    }
+
+    return 0;
+}
